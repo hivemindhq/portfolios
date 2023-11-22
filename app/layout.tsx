b2543ad148125a20366e3150d9c2812f86c949fc
polargh <io@itspolar.dev>
2023-11-21 18:29:29 -0600

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
@@ -14,6 +16,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const date = new Date().getFullYear();
   return (
     <html lang="en">
       <body
@@ -24,6 +27,20 @@ export default function RootLayout({
       >
         <Toaster richColors />
         {children}
+        <div className="max-w-screen-xl mx-auto w-full px-4 my-5">
+          <div className="flex items-center justify-between">
+            <a href="https://hivemindrobotics.net/">
+              <div className="opacity-40 underline hover:opacity-60 transition">
+                &copy; {date}, hivemind
+              </div>
+            </a>
+
+            <img
+              className="opacity-40"
+              src="https://hits-app.vercel.app/hits?url=https://portfolios.hivemindrobotics.net"
+            />
+          </div>
+        </div>
       </body>
     </html>
   );
