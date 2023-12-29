f7c6c03af502979c19e88f602f3b05d87987b0e6
polargh <io@itspolar.dev>
2023-12-29 13:20:17 -0600

    tos

---
+++ app/terms/page.tsx
@@ -0,0 +1,135 @@
+export default function PrivacyPolicy() {
+	return (
+		<>
+			<div className="container mx-auto py-8 md:py-12 lg:py-24 max-w-4xl">
+				<div className="space-y-3">
+					<h1 className="text-2xl font-bold">Terms of Service</h1>
+					<em>
+						<p className="opacity-70">Last updated: 12/29/2023</p>
+					</em>
+				</div>
+				<div className="my-12 space-y-4">
+					<h2 className="text-xl font-semibold">Introduction</h2>
+					<p className="opacity-70">
+						Welcome to <strong>Portfolios</strong>! As you have just clicked on our Terms of
+						Service, please pause, grab something to drink, and carefully read the following pages.
+						It should take you less than 20 minutes.
+					</p>
+					<p className="opacity-70">
+						These Terms of Service (&ldquo;<strong>Terms</strong>&ldquo;, &ldquo;
+						<strong>Terms of Service</strong>&ldquo;) govern your use of our web pages located at{' '}
+						<a
+							href="https://portfolios.hivemindrobotics.net/"
+							className="underline hover:opacity-70 transition-all"
+						>
+							portfolios.hivemindrobotics.net
+						</a>{' '}
+						and other distributions of Portfolios (together or individually as &ldquo;
+						<strong>Service</strong>&ldquo;) operated by Hivemind Robotics.
+					</p>
+					<p className="opacity-70">
+						Our Privacy Policy also governs your use of our Service and explains how we collect,
+						safeguard, and disclose information that results from your use of our web pages. Please
+						read it{' '}
+						<a href="/privacy" className="underline hover:opacity-70 transition-all">
+							here
+						</a>
+						.
+					</p>
+					<p className="opacity-70">
+						Your agreement with us includes these Terms and our Privacy Policy (&ldquo;
+						<strong>Agreements</strong>&ldquo;) You acknowledge that you have read and understood
+						these Agreements, and are bound to them.
+					</p>
+					<p className="opacity-70">
+						If you do not agree with (or cannot comply with) Agreements, then you may not use the
+						Service, but please let us know why by emailing us at{' '}
+						<a
+							href="mailto:webm@hivemindrobotics.net"
+							className="underline hover:opacity-70 transition-all"
+						>
+							webm@hivemindrobotics.net
+						</a>{' '}
+						or by using the contact form on our{' '}
+						<a
+							target="_blank"
+							href="https://hivemindrobotics.net/team#contact"
+							className="underline hover:opacity-70 transition-all"
+						>
+							main site
+						</a>
+						.
+					</p>
+					<p>Thank you for being responsible.</p>
+					<h2 className="text-xl font-semibold py-4">Communications</h2>
+					<p className="opacity-70">
+						By creating an account on our Service, you agree to subscribe to newsletters, marketing
+						or promotional materials and other information we may send. However, you may opt out
+						from recieving any, or all of these communications by following the unsubscribe link.
+					</p>
+					<h2 className="text-xl font-semibold py-4">Content</h2>
+					<p className="opacity-70">
+						Content found on this service (with the exception of third-party content provided by
+						users) is either owned by Hivemind Robotics, or is used with permission. You may
+						distribute your own version of Portfolios as it is open source, however you must provide
+						proper attribution or we have full right to send a take-down request. You may not use
+						this service for commercial gain, without express advance written permission from us, or
+						the respective owner.
+					</p>
+					<h3 className="text-md font-bold pb-2">Third-Party User Provided Content</h3>
+					<p className="opacity-70">
+						Users are encouraged to submit <b>reports</b> concerning content they believe warrants
+						attention. In instances where users claim ownership of reported content, the provision
+						of supporting evidence is required to establish ownership rights.
+					</p>
+					<h2 className="text-xl font-bold py-4">Prohibited Uses</h2>
+					<p className="opacity-70">
+						You may use Service only for lawful purposes and in accordance with Terms. You agree not
+						to use Service:
+						<ul className="list-disc ms-4 my-5 space-y-3">
+							<li>
+								In any way that violates any applicable national or international law or regulation.
+							</li>
+							<li>
+								For the purpose of exploiting, harming, or attempting to exploit or harm minors in
+								any way by exposing them to inappropriate content or otherwise.
+							</li>
+							<li>
+								To impersonate or attempt to impersonate Company, a Company employee, another user,
+								or any other person or entity.
+							</li>
+							<li>
+								To leak confidential documentation of any Team or Company without ownership or
+								permission from it&apos;s owner.
+							</li>
+							<li>
+								In any way that infringes upon the rights of others, or in any way is illegal,
+								threatening, fraudulent, or harmful, or in connection with any unlawful, illegal,
+								fraudulent, or harmful purpose or activity.
+							</li>
+						</ul>
+					</p>
+					<h2 className="text-xl font-bold py-4">Contact Us</h2>
+					<p className="opacity-70">
+						If you have any questions or concerns about this privacy policy, please contact us by
+						email at{' '}
+						<a
+							href="mailto:webm@hivemindrobotics.net"
+							className="underline hover:opacity-70 transition-all"
+						>
+							webm@hivemindrobotics.net
+						</a>{' '}
+						or by using the contact form on our{' '}
+						<a
+							target="_blank"
+							href="https://hivemindrobotics.net/team#contact"
+							className="underline hover:opacity-70 transition-all"
+						>
+							main site
+						</a>
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
