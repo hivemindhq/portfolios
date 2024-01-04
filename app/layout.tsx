542d28c242bf98977ff673c4305d3e91ff51fc41
polargh <io@itspolar.dev>
2024-01-04 11:55:27 -0600

    im dumb

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 			<head>
 				<PlausibleProvider
 					domain="portfolios.hivemindrobotics.net"
-					customDomain="lab.itzpolar.me"
+					customDomain="https://lab.itzpolar.me"
 					selfHosted
 				/>
 			</head>
