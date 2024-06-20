9013633ab570d80e5bc540167e2977a8bd975336
polargh <io@itspolar.dev>
2024-06-20 15:44:43 -0500

    403 http

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,15 @@ import './globals.scss';
 import {GeistSans} from 'geist/font/sans';
 import {cn} from '@/lib/utils';
 import {PHProvider} from '@/server/posthog';
-import PlausibleProvider from 'next-plausible'
+import PlausibleProvider from 'next-plausible';
 import Navbar from '@/components/navbar';
 import SiteNav from '@/components/site-nav';
-import {Toaster} from 'react-hot-toast'
+import {Toaster} from 'react-hot-toast';
 import {ThemeProvider} from '@/components/theme-provider';
 import Footer from '@/components/footer';
 
 import localFont from 'next/font/local';
-import { Outfit } from 'next/font/google'
+import {Outfit} from 'next/font/google';
 
 export const metadata: Metadata = {
 	title: 'Portfolios',
@@ -19,9 +19,8 @@ export const metadata: Metadata = {
 };
 
 const fontSans = Outfit({
-	subsets: ["latin"],
-})
-
+	subsets: ['latin'],
+});
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
 	return (
@@ -37,10 +36,18 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
 			</head>
 			<body className={cn('min-h-screen bg-background antialiased', fontSans.className)}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-						<SiteNav />
+					{/* <SiteNav />
 						{children}
 						<Toaster />
-						<Footer />
+						<Footer /> */}
+					<div className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+						<div className="m-auto text-center space-y-4 ">
+							<h1 className="text-2xl font-semibold">403</h1>
+							<p>
+								<b>Forbidden</b>, we are currently undergoing a maintenance period, please try again later.
+							</p>
+						</div>
+					</div>
 				</ThemeProvider>
 			</body>
 		</html>
