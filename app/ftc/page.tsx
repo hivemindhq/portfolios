e5494b03ec341f9aa0d310e5e7341bae9e2f6e85
polargh <io@itspolar.dev>
2024-06-20 15:20:11 -0500

    fix: iterator & &apos;

--- app/ftc/page.tsx
+++ app/ftc/page.tsx
@@ -12,8 +12,8 @@ export default function FTCPage() {
 		<div className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
 			<div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 gap-4">
 				{portfolios?.map(portfolio => (
-					<Link href={portfolio.s3_url ? portfolio.s3_url : ''} target="_blank">
-						<div key={portfolio.id} className="flex group">
+					<Link key={portfolio.id} href={portfolio.s3_url ? portfolio.s3_url : ''} target="_blank">
+						<div className="flex group">
 							<div className="p-1 mx-auto">
 								<div className="group-hover:scale-[0.95] ease-out duration-300 transition-all group-hover:opacity-70">
 									<div className="flex aspect-square items-center justify-center group">
